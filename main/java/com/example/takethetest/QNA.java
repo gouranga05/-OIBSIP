2a472c42bcc6677df44d208db806ceaabd7494df
gouranga05 <109362222+gouranga05@users.noreply.github.com>
2023-03-10 23:20:26 +0530

    Add files via upload

---
+++ main/java/com/example/takethetest/QNA.java
@@ -0,0 +1,20 @@
+package com.example.takethetest;
+
+public class QNA {
+    public static String []questions={
+            "Which of the following is a service based Company?",
+            "Which of the following is not a programming language>",
+            "Which is a Streaming platform? "
+
+    };
+    public static String [][]options={
+            {"Apple","Google","TCS","Amazon"},
+            {"Java","Dart","Ruby","Hindi"},
+            {"Hotstar","Github","Eclipse","Notepad"}
+    };
+    public static String []answers={
+            "TCS",
+            "Hindi",
+            "Hotstar"
+    };
+}
